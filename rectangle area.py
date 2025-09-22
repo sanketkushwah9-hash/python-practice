5eec70292d80baf6322bde34e08ac0de15323a9b
sanketkushwah9-hash <sanketkushwah9@gmail.com>
2025-09-22 18:34:24 +0530

    Add files via upload

---
+++ rectangle area.py
@@ -0,0 +1,4 @@
+lenght=float(input("enter lenght: "))
+width=float(input("enter width: "))
+area=lenght*width
+print("area of rectangle=",area)
---
+++ temperature.py
@@ -0,0 +1,3 @@
+celsius=float(input("enter temperature in celsius: "))
+fahrenheit=(celsius*9/5)+32
+print("temperature in fahrengeit =",fahrenheit)
